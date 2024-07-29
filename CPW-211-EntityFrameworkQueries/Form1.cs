c2a558022055002e5ba697e398d9e8c0a1221c42
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-29 00:00:43 -0700

    Added join example

--- CPW-211-EntityFrameworkQueries/Form1.cs
+++ CPW-211-EntityFrameworkQueries/Form1.cs
@@ -1,5 +1,6 @@
 using CPW_211_EntityFrameworkQueries.Data;
 using CPW_211_EntityFrameworkQueries.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Text;
 
 namespace CPW_211_EntityFrameworkQueries
@@ -65,22 +66,53 @@ namespace CPW_211_EntityFrameworkQueries
 
             // Check if a Vendor exists in Washington
             bool doesExist = (from v in dbContext.Vendors
-                             where v.VendorState == "WA"
-                             select v).Any();
+                              where v.VendorState == "WA"
+                              select v).Any();
 
             // Get number of Invoices
             int invoiceCount = (from invoice in dbContext.Invoices
-                               select invoice).Count();
+                                select invoice).Count();
 
             // Query a single vendor
             Vendor? singleVendor = (from v in dbContext.Vendors
-                         where v.VendorName == "IBM"
-                         select v).SingleOrDefault();
+                                    where v.VendorName == "IBM"
+                                    select v).SingleOrDefault();
 
             if (singleVendor == null)
             {
                 // Do something with the Vendor object
             }
+        }
+
+        private void btnVendorsAndInvoices_Click(object sender, EventArgs e)
+        {
+            ApContext dbContext = new();
+
+            // Vendors LEFT JOIN Invoices
+            List<Vendor> allVendors = dbContext.Vendors.Include(v => v.Invoices).ToList();
+
+            // Unfinished code: This pulls a vendor obkect for each individual invoice, vendors
+            // are also pulled back if they have no invoices
+            //List<Vendor> allVendors = (from v in dbContext.Vendors
+            //                          join inv in dbContext.Invoices
+            //                            on v.VendorId equals inv.VendorId into grouping
+            //                          from inv in grouping.DefaultIfEmpty()
+            //                          select v).ToList();
+
+            StringBuilder results = new();
+
+            foreach (Vendor v in allVendors)
+            {
+                results.Append(v.VendorName);
+                foreach (Invoice inv in v.Invoices)
+                {
+                    results.Append(", ");
+                    results.Append(inv.InvoiceNumber);
+                }
+                results.AppendLine();
+            }
+
+            MessageBox.Show(results.ToString());
         }
     }
 
