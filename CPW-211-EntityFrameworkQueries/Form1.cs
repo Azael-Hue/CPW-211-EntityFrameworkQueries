413f79f9195c8503c5995ad8229ffba801e4e54c
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-28 21:03:47 -0700

    Added example to select all vendors

--- CPW-211-EntityFrameworkQueries/Form1.cs
+++ CPW-211-EntityFrameworkQueries/Form1.cs
@@ -1,3 +1,6 @@
+using CPW_211_EntityFrameworkQueries.Data;
+using CPW_211_EntityFrameworkQueries.Models;
+
 namespace CPW_211_EntityFrameworkQueries
 {
     public partial class Form1 : Form
@@ -5,6 +8,18 @@ namespace CPW_211_EntityFrameworkQueries
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectAllVendors_Click(object sender, EventArgs e)
+        {
+            using ApContext dbContext = new ApContext();
+
+            // LINQ (Language Integrated Query) method syntax
+            List<Vendor> vendorList = dbContext.Vendors.ToList();
+
+            // LINQ query syntax
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                        select v).ToList();
         }
     }
 }
